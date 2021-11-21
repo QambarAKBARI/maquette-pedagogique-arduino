0c81d2c183cffcc66bc1f97ec0071ba1b6a56e0f
QambarAKBARI <qambarakbarifr@gmail.com>
2021-11-21 19:13:26 +0100

    Ajout de fichier zero.h

---
+++ zero.h
@@ -0,0 +1,15 @@
+#pragma once
+bool Moteur_config(String Moteur);
+bool Moteur(String Moteur, String Pilotage);
+
+bool Bouton(String bouton);
+bool fdc(String fdc);
+
+bool Recepteur_config (String Recepteur);
+bool Recepteur(String recepteur);
+
+bool led_config (String led);
+bool Led (String led, String Pilotage);
+
+bool Buzzer_config(String buzzer);
+bool Buzzer(String Pilotage);
